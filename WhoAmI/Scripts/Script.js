c00448b1522179b9acb8da56e1a924e599f88c6f
yrohithreddy <yrohithreddy@gmail.com>
2017-06-07 22:49:25 -0600

    first commit

---
+++ WhoAmI/Scripts/Script.js
@@ -0,0 +1,28 @@
+﻿/// <reference path="angular.min.js" />
+/// <reference path="angular-route.min.js" />
+
+
+//Module
+var myApp = angular
+            .module("myModule", [])
+            .config(function ($routeProvider) {
+                $routeProvider
+                    .when("/home",{
+                        templateUrl: "Templates/Home.html",
+                        controller:  "homeController"
+                    })
+                    .when("/about",{
+                        templateUrl: "Templates/About.html",
+                        controller:  "aboutController"
+                    })
+                    .when("/resume",{
+                        templateUrl: "Templates/Resume.html",
+                        controller:  "resumeController"
+                    })
+            })
+            .controller("homeController",function($scope, $location,$anchorScroll){
+                var currentVariable = this;
+                currentVariable.message = "Hello world";
+            })
+            
+            
